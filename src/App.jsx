09e98039f8f17c707e25ce02470bc5d5dc7c357a
Mayya Matueva <mayamatueva@gmail.com>
2024-02-19 11:51:04 +0100

    add a new component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import Navbar from "./Navbar";
+//PascalCase or CamelCase
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Navbar />
+      <h1>This is my first App!</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>This is the content of my app</p>
+        <section>This is a section</section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
+  // const html = "scriv koden här";
+  // return html;
 }
 
-export default App
+// const App =() => {
+//   return <div>This is the app component</div>
+// };
+export default App;
